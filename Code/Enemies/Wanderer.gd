1a83d20f7e721f42a92d7aa49fa5837769091442
PaulGGJ <paulsmart525@gmail.com>
2026-01-31 20:41:04 -0600

    mook flip
    Mook now faces the direction its moving

--- Code/Enemies/Wanderer.gd
+++ Code/Enemies/Wanderer.gd
@@ -15,6 +15,10 @@ func _physics_process(delta):
 	if !is_on_floor():
 		velocity = velocity + get_gravity() * delta
 	move_and_slide()
+	if currDir == 1:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
 
 func _process(delta):
 	# Guards
@@ -23,7 +27,6 @@ func _process(delta):
 	
 	# Proceed
 	var foundMove = false
-	var prevDir = currDir
 	if currDir == LEFT:
 		if %EdgeCheckerL.is_colliding():
 			foundMove = true
@@ -34,19 +37,16 @@ func _process(delta):
 			foundMove = true
 		else:
 			currDir *= -1 # Try the other way
-	if currDir != prevDir:
-		print("Enemy changed")
-		UpdateSprites()
 	if foundMove && !punched:
 		velocity.x = currDir * wanderSpeed
 
-func UpdateSprites():
-		if currDir == LEFT:
-			facing = FACING.LEFT
-		else:
-			facing = FACING.RIGHT
-		$AnimatedSprite2D.sprite.flip_h = false if facing == FACING.LEFT else true
-		print($AnimatedSprite2D.sprite.flip_h)
+#func UpdateSprites():
+#		if currDir == LEFT:
+#			facing = FACING.LEFT
+#		else:
+#			facing = FACING.RIGHT
+#		$AnimatedSprite2D.sprite.flip_h = false if facing == FACING.LEFT else true
+#		print($AnimatedSprite2D.sprite.flip_h)
 	
 
 func _on_wall_finder_right_body_entered(body: Node2D) -> void:
