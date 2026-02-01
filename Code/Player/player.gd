83bd0e52552025eb5650566d4781e586ce44c44f
PaulGGJ <paulsmart525@gmail.com>
2026-02-01 13:43:22 -0600

    Non-climbable tiles
    Created a blue tileset the spider can't climb

--- Code/Player/player.gd
+++ Code/Player/player.gd
@@ -294,6 +294,7 @@ func diag_finished() -> void:
 func custom_on_ceiling():
 	var bodies = %SpiderCeiling.get_overlapping_bodies()
 	for body in bodies:
+		print(body)
 		if body.get_class() == "TileMap" && !stunned && (Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right") || crawling):
 			return true
 	return false
