ead96daca326b3e614dcfca09df24ae76f6970fa
Parmeisan <Parmeisan@users.noreply.github.com>
2026-02-09 18:30:30 -0600

    Added a health bar

---
+++ Code/Player/HealthBar.gd
@@ -0,0 +1,27 @@
+extends CanvasItem
+class_name HealthBar
+
+var MAX_HEALTH : int = 2
+
+func SetMaxHealth(max : int):
+	MAX_HEALTH = max
+	SetHealth(MAX_HEALTH)
+
+func SetHealth(h : int) -> void:
+	print("Setting health to %d" % MAX_HEALTH)
+	#for obj in %HealthContainer.get_children():
+		#.remove_child()
+	while %HealthContainer.get_child_count() > 0:
+		%HealthContainer.remove_child(%HealthContainer.get_child(0))
+	
+	for i in range(0, h):
+		AddHeart(%TemplateFull)
+	for i in range(h, MAX_HEALTH):
+		AddHeart(%TemplateEmpty)
+	
+func AddHeart(template):
+	print("Adding heart")
+	var heart = template.duplicate()
+	heart.visible = true
+	%HealthContainer.add_child(heart)
+		
